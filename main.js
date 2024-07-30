3ca482f611e21973f9d9fc4e5e69c4d2d9b18ebb
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-07-30 07:42:15 -0300

    c

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const botoes = document.querySelectorAll('.parametro-senha__botao')
 
 botoes[0].onclick = diminuirTamanho
 
+let tamanhoSenha = 5
+
 function diminuirTamanho() {
     console.log('teste');
 }
-numeroSenha.textContent = 6
+numeroSenha.textContent = tamanhoSenha
