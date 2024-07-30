b0b6879d1d564a92f76cc9b23a88a5b9f791eab8
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-07-30 07:33:40 -0300

    bh

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto"')
+const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
 botoes[0].onclick = diminuirTamanho
