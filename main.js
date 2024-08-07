5ea7149bc4232ea783de87e1321c92e9bb370a0f
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-08-07 09:41:09 -0300

    x

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ function almentarTamanho() {
 function geraSenha(){
     let senha = ""
     for(let i = 0; i < tamanhoSenha; i++ ){
-        let numeroAleatorio = Math.floor(numeroAleatorio)
+        let numeroAleatorio = Math.random() * maiuscula.length
+        numeroAleatorio = Math.floor(numeroAleatorio)
         senha = senha + maiuscula[numeroAleatorio];
         console.log(senha)
     }
